ad9c02686a975643bc07646f6c2cf162d08c6b90
Deivy <dpaul_20@hotmail.com>
2022-03-11 21:46:25 -0300

    Section 9: Configuration Management

---
+++ src/config.schema.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().default(3000).required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+});
